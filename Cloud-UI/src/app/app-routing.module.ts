ee27fff9cc9ca72c716873652b50662404ada461
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-06-25 01:20:25 +0200

    [UI] Add blank route

--- Cloud-UI/src/app/app-routing.module.ts
+++ Cloud-UI/src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import {LoginView} from "./views/login/login.view";
 import {AuthGuard} from "./guards/auth.guard";
 import {DashboardView} from "./views/dashboard/dashboard.view";
 import {ProxyView} from "./views/management/proxy/proxy.view";
+import {LoadingComponent} from "./components/loading/loading.component";
 
 const routes: Routes = [
     /**
      * Global
      */
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'blank', component: LoadingComponent},
     {path: 'login', component: LoginView},
     {path: 'dashboard', component: DashboardView, canActivate: [AuthGuard]},
+
 
     /**
      * Management
