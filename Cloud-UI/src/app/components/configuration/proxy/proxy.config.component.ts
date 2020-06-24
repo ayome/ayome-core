9f4eac7062024f52bbf5c87182c9d65e3321bc08
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-06-25 01:20:55 +0200

    [UI] Add proxy remove

--- Cloud-UI/src/app/components/configuration/proxy/proxy.config.component.ts
+++ Cloud-UI/src/app/components/configuration/proxy/proxy.config.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ProxyService} from "../../../services/manage/proxy.service";
 import {SocketService} from "../../../services/socket.service";
+import {Router} from "@angular/router";
+import {AlertService} from "../../../services/alert.service";
 
 @Component({
     selector: 'proxy-config',
@@ -16,7 +18,9 @@ export class ProxyConfigComponent implements OnInit {
 
     constructor(
         private proxyService: ProxyService,
-        private socketService: SocketService
+        private socketService: SocketService,
+        private alertService: AlertService,
+        private router: Router
     ) {
     }
 
@@ -37,8 +41,6 @@ export class ProxyConfigComponent implements OnInit {
 
     getConfig() {
         this.socketService.emit("/manage/proxy/config/get", {name: "default"}).then((data: any) => {
-
-            console.log(data)
             this.configMemory = data.inspect.hostConfig.memory / (1024 * 1024)
         })
     }
@@ -51,10 +53,63 @@ export class ProxyConfigComponent implements OnInit {
                 name: "default",
                 memory: this.configMemory * (1024 * 1024)
             }
-        ).then((res: any) => {
+        ).then(() => {
             this.loading = false
-            console.log(res)
             this.proxyService.hideConfig()
+        })
+    }
+
+    removeQuestion() {
+        this.alertService.show({
+            loading: false,
+            content: "Are you sure you want to remove the proxy server? " +
+                "It will only deleted in the system. " +
+                "No folders or files will be touched, you have to delete the proxy folder manually. " +
+                "You can simply click the install button again without data lose (like an bungeecord update).",
+            btnText: "Remove it",
+            callback: () => {
+                this.removeProxy()
+            }
+        })
+    }
+
+    removeProxy() {
+        this.loading = true
+
+        this.proxyService.hideConfig()
+        this.alertService.show({
+            loading: true,
+            content: "Removing proxy server",
+            btnText: "",
+            callback: null
+        })
+
+        this.socketService.emit(
+            "/manage/proxy/remove",
+            {
+                name: "default"
+            }
+        ).then((res: any) => {
+            if (res.success) {
+                this.alertService.show({
+                    loading: false,
+                    content: "The proxy server has been removed",
+                    btnText: "ok",
+                    callback: () => {
+                        this.alertService.hide()
+                        this.router.navigateByUrl("/blank").then(() => this.router.navigateByUrl("/manage/proxy"))
+                    }
+                })
+            } else {
+                this.alertService.show({
+                    loading: false,
+                    content: "",
+                    btnText: "ok",
+                    callback: () => {
+                        this.alertService.hide()
+                    }
+                })
+            }
         })
     }
 }
