cb89ff711c6d93ba321a27322a3900b347db5a0d
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-06-27 06:54:03 +0200

    [UI] Add static-servers table

--- Cloud-UI/src/app/views/management/static/static.view.ts
+++ Cloud-UI/src/app/views/management/static/static.view.ts
@@ -5,6 +5,7 @@ import {AlertService} from "../../../services/alert.service";
 import {chart} from "../../../components/charts/proxy.chart";
 import {ChartComponent} from "ng-apexcharts";
 import anime from "assets/js/anime.min";
+import {StaticService} from "../../../services/manage/static.service";
 
 @Component({
     selector: 'app-first',
@@ -14,12 +15,28 @@ import anime from "assets/js/anime.min";
 export class StaticView implements OnInit {
     public loading = true
 
+    public list = []
+
     constructor(
+        private staticService: StaticService,
         private socket: SocketService,
         private alertService: AlertService
     ) {
     }
 
     async ngOnInit() {
+        this.getServers()
+    }
+
+    getServers() {
+        this.socket.emit("/manage/static/list").then((data: any) => {
+            this.loading = false
+            console.log(data)
+            this.list = data.servers
+        })
+    }
+
+    addServer() {
+        this.staticService.showConfig()
     }
 }
