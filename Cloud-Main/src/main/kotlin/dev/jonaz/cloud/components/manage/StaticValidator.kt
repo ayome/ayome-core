3a7491eb580f9d0d4ae0b5feef65e9f37978e5fc
Jonas <48187105+jonaznas@users.noreply.github.com>
2020-06-27 06:58:52 +0200

    [MAIN] Add static validation

---
+++ Cloud-Main/src/main/kotlin/dev/jonaz/cloud/components/manage/StaticValidator.kt
@@ -0,0 +1,29 @@
+package dev.jonaz.cloud.components.manage
+
+import dev.jonaz.cloud.model.DatabaseModel
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class StaticValidator : DatabaseModel() {
+
+    fun validateName(name: String): Pair<Boolean, String?> {
+        if (Regex("[a-zA-Z0-9-_]+\$").matches(name).not()) {
+            return Pair(false, "Can only contain alphanumeric and dashes")
+        }
+
+        when (name.length) {
+            in 16..Int.MAX_VALUE -> return Pair(false, "Cannot be longer than 16 characters")
+            in Int.MIN_VALUE..3 -> return Pair(false, "Cannot be shorter than 4 characters")
+        }
+
+        if(exist(name)) {
+            return Pair(false, "The name already exist")
+        }
+
+        return Pair(true, null)
+    }
+
+    private fun exist(name: String) = transaction {
+        Static.select { Static.name eq name }.toList().isNotEmpty()
+    }
+}
