22be124ef09cb155a2c864813b369291db9df10b
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-06 03:53:23 +0200

    [MAIN] Add get method to StaticManager

--- Cloud-Main/src/main/kotlin/dev/jonaz/cloud/components/manage/StaticManager.kt
+++ Cloud-Main/src/main/kotlin/dev/jonaz/cloud/components/manage/StaticManager.kt
@@ -1,14 +1,18 @@
 package dev.jonaz.cloud.components.manage
 
 import dev.jonaz.cloud.model.DatabaseModel
+import dev.jonaz.cloud.model.docker.DockerInspectModel
 import dev.jonaz.cloud.model.server.StaticServerModel
 import dev.jonaz.cloud.util.docker.container.DockerContainer
+import dev.jonaz.cloud.util.docker.container.DockerInspect
 import dev.jonaz.cloud.util.system.ErrorLogging
 import dev.jonaz.cloud.util.system.SystemPathManager
 import dev.jonaz.cloud.util.system.SystemRuntime
 import dev.jonaz.cloud.util.system.filesystem.DirectoryManager
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -26,6 +30,22 @@ class StaticManager : DatabaseModel() {
         }
 
         return@transaction servers
+    }
+
+    fun getStatic(name: String): Pair<Boolean, DockerInspectModel?> {
+        val staticName = "cloud-static-$name"
+
+        val statics = transaction {
+            Static.select { Static.name eq name }.toList()
+        }
+
+        if (statics.isEmpty()) {
+            return Pair(false, null)
+        }
+
+        val result = DockerInspect().getByName(staticName)
+
+        return Pair(result.first, result.second)
     }
 
     fun installStatic(name: String, memory: Long, port: Int, version: String): Boolean {
