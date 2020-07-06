2401ba46fbce303f84e9280b736a034d929c9df4
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-06 03:56:21 +0200

    [UI] Add get, stop, start, sendCommand to static service

--- Cloud-UI/src/app/services/manage/static.service.ts
+++ Cloud-UI/src/app/services/manage/static.service.ts
@@ -15,6 +15,39 @@ export class StaticService {
     constructor(private socket: SocketService) {
     }
 
+    get(name) {
+        return new Promise(resolve => {
+            this.socket.emit("/manage/static/get", {
+                name: name
+            }).then(resolve)
+        })
+    }
+
+    stop(name) {
+        return new Promise(resolve => {
+            this.socket.emit("/manage/static/stop", {
+                name: name
+            }).then(resolve)
+        })
+    }
+
+    start(name) {
+        return new Promise(resolve => {
+            this.socket.emit("/manage/static/start", {
+                name: name
+            }).then(resolve)
+        })
+    }
+
+    sendCommand(name, command) {
+        return new Promise(resolve => {
+            this.socket.emit("/manage/static/command", {
+                name: name,
+                command: command
+            }).then(resolve)
+        })
+    }
+
     showCreateModal() {
         this.showModalCreate.next()
     }
