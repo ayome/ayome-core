0282f8464b3dfb0bdb8e399a159e1ae41e05758d
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-07 13:50:20 +0200

    [MAIN] Optimize DockerLogs

--- Cloud-Main/src/main/kotlin/dev/jonaz/cloud/util/docker/container/DockerLogs.kt
+++ Cloud-Main/src/main/kotlin/dev/jonaz/cloud/util/docker/container/DockerLogs.kt
@@ -1,37 +1,43 @@
 package dev.jonaz.cloud.util.docker.container
 
 import dev.jonaz.cloud.util.socket.SocketServer
-import dev.jonaz.cloud.util.system.SystemRuntime
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.io.BufferedReader
 
 class DockerLogs {
 
     fun getLogs(containerName: String, limit: Int): List<String> {
-        val process = SystemRuntime().execRaw("docker logs --tail $limit $containerName")
-        val bufferedReader = process.inputStream.bufferedReader()
-
+        val process = ProcessBuilder("docker", "logs", "--tail", limit.toString(), containerName)
+            .redirectErrorStream(true)
+            .start()
         val result = mutableListOf<String>()
-        bufferedReader.forEachLine { s ->
-            when {
-                s.isEmpty() -> return@forEachLine
-                s.equals(">") -> return@forEachLine
-                else -> result.add(s)
-            }
-        }
-        return result
-    }
 
-    fun startLoggingToChannel(containerName: String, channelName: String, eventName: String) = GlobalScope.launch {
-        val inputStream = SystemRuntime().execRaw("docker logs -f --tail 100 $containerName").inputStream
-        val bufferedReader = inputStream.bufferedReader()
-
-        bufferedReader.forEachLine line@{ s ->
+        fun addToResult(reader: BufferedReader) = reader.forEachLine line@{ s ->
             when {
                 s.isEmpty() -> return@line
                 s.equals(">") -> return@line
-                else -> {
-                    SocketServer.server.getRoomOperations(channelName).sendEvent(eventName, s)
+                else -> result.add(s)
+            }
+        }
+
+        addToResult(process.inputStream.bufferedReader())
+
+        return result
+    }
+
+    fun startLoggingToChannel(containerName: String, channelName: String, eventName: String) {
+        val process = ProcessBuilder("docker", "logs", "-f", "--tail", "100", containerName)
+            .redirectErrorStream(true)
+            .start()
+        val reader = process.inputStream.bufferedReader()
+
+        GlobalScope.launch {
+            reader.forEachLine line@{ s ->
+                when {
+                    s.isEmpty() -> return@line
+                    s.equals(">") -> return@line
+                    else -> SocketServer.server.getRoomOperations(channelName).sendEvent(eventName, s)
                 }
             }
         }
