3b56171acc26ae656839b80e341af46f1964a698
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-15 17:30:31 +0200

    [MAIN] Add timeout to DockerExec

--- Cloud-Main/src/main/kotlin/dev/jonaz/cloud/util/docker/system/DockerExec.kt
+++ Cloud-Main/src/main/kotlin/dev/jonaz/cloud/util/docker/system/DockerExec.kt
@@ -2,10 +2,11 @@ package dev.jonaz.cloud.util.docker.system
 
 import com.pty4j.PtyProcess
 import java.io.PrintWriter
+import java.util.concurrent.TimeUnit
 
 class DockerExec {
 
-    fun pty(container: String, command: String) {
+    fun pty(container: String, command: String, timeout: Int = 0) {
         val cmd = arrayOf("docker", "exec", "-it", container, "/bin/sh")
 
         val pty = PtyProcess.exec(cmd)
@@ -15,7 +16,9 @@ class DockerExec {
         pw.println(command)
         pw.close()
 
-        pty.waitFor()
+        if (timeout != 0) pty.waitFor(timeout.toLong(), TimeUnit.MILLISECONDS)
+        else pty.waitFor()
+
         pty.destroyForcibly()
     }
 }
