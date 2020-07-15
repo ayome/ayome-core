30129b64665a22d94487e21673d4a3db4367f599
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-15 17:30:17 +0200

    [MAIN] Add PropertiesParser

---
+++ Cloud-Main/src/main/kotlin/dev/jonaz/cloud/util/system/filesystem/PropertiesParser.kt
@@ -0,0 +1,17 @@
+package dev.jonaz.cloud.util.system.filesystem
+
+import dev.jonaz.cloud.util.system.SystemPathManager
+import java.util.*
+
+class PropertiesParser {
+
+    fun fromCloudFile(path: String): Properties {
+        val properties = Properties()
+        val filePath = SystemPathManager.current + path
+        val resourceStream = this::class.java.classLoader.getResourceAsStream(filePath)
+
+        properties.load(resourceStream)
+
+        return properties
+    }
+}
