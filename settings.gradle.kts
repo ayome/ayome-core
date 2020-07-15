befc3a5d00a9612f57348fe25fb1c84fe46ddf43
jonaznas <48187105+jonaznas@users.noreply.github.com>
2020-07-15 17:22:43 +0200

    [MAIN] Add internal sub-projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
 rootProject.name = "Cloud"
 
-include("Cloud-Main", "Cloud-UI")
+include(
+    "Cloud-Main",
+    "Cloud-UI",
+    "Cloud-Internal:Proxy-Cloud-Addon",
+    "Cloud-Internal:Static-Cloud-Addon"
+)
